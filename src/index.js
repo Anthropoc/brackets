d650501868e6e436d86126d80efbd7197c368570
DESKTOP-O81LPDS\Anthropoc <anthropocmaxi@gmail.com>
2020-10-15 13:20:56 +0300

    feat: modified: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if(str.length % 2 != 0 || str.length == 0) {
+        return false;
+    } else {
+        for(let i = 0; i < bracketsConfig.length; ){
+            let bracketKind = bracketsConfig[i].join('');
+            if(str.includes(bracketKind)){
+                str = str.replace(bracketKind, '');
+                i = 0;
+            } else {
+                i = i + 1;
+            }
+        };
+        if(str.length) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
+
